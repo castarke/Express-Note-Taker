11dfabb92e803cd1cdbfca1835a41daf9772f97d
castarke <candler.starke@yahoo.com>
2023-04-09 19:13:12 -0400

    fixed the delete function

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -24,31 +24,12 @@ router.post('/notes', (req,res)=>{
 })
 
 router.delete('/notes/:id', (req, res) => {
-    const id = uuidv4(req.params.id);
+    const id = req.params.id;
     const data = fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf8');
     const notes = JSON.parse(data);
     const newNotes = notes.filter((note) => note.id !== id);
     fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(newNotes));
     res.json({ msg: 'deleted successfully' });
   });
-
-// router.delete('/notes/:id' ,  (req, res)=>{
-//         let id = req.params.id;
-//         let data =readFromFile('../db/db.json');
-//         let notes = JSON.parse(data);
-//         let newNotes = notes.filter((notes) => notes.id !== id);
-//        writeToFile('../db/db.json', JSON.stringify(newNotes))
-//         res.json({msg: 'deleted successfully'})
-//     })
-
-
-//fs readfile to get the currernt array of objects
-//parse old array
-//get new object from req.body
-//push new object to old array
-//strigify new array
-//fs write file with new array
-//res.send new array 
-
 
 module.exports = router;
